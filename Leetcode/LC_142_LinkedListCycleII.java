cb2cdc324e950c22b391bbdce7f330ebc6227a4d
Katie <raokatie324@gmail.com>
2016-12-04 16:47:28 -0500

    Add leetcode 142. Linked List CycleII

---
+++ Leetcode/LC_142_LinkedListCycleII.java
@@ -0,0 +1,34 @@
+//142. Linked List Cycle II
+//Given a linked list, return the node where the cycle begins. If there is no cycle, return null.
+
+
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+        while(fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow) {
+                ListNode slow2 = head;
+                while(slow2 != slow) {
+                    slow2 = slow2.next;
+                    slow = slow.next;
+                }
+                return slow2;
+            }
+        }
+        return null;
+    }
+}
