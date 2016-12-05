b9d31d7ac6763585410d6aaa368c5f7571291944
Katie <raokatie324@gmail.com>
2016-12-04 23:00:12 -0500

    Add leetcode 266. Palindrome Permutation

---
+++ Leetcode/LC_266_PalindromePermutation.java
@@ -0,0 +1,21 @@
+//266. Palindrome Permutation
+//Given a string, determine if a permutation of the string could form a palindrome.
+//For example,
+//“code" -> False, "aab" -> True, "carerac" -> True.
+
+
+//Just count the appearances of the characters. if odd appearances are greater than 1, then return false;
+public class Solution {
+    public boolean canPermutePalindrome(String s) {
+        int[] array = new int[256];
+        for(char c : s.toCharArray()) {
+            array[c]++;
+        }
+        int count = 0;
+        for (int x : array) {
+            if (x % 2 == 1) count++;
+            if (count > 1) return false;
+        }
+        return true;
+    }
+}
