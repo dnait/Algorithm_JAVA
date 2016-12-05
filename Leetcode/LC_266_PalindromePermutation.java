d97182b1088cb25035343079904b55584ac1f2af
Katie <raokatie324@gmail.com>
2016-12-04 23:44:55 -0500

    Add leetcode 77. Combinations

--- Leetcode/LC_266_PalindromePermutation.java
+++ Leetcode/LC_266_PalindromePermutation.java
@@ -3,6 +3,20 @@
 //For example,
 //“code" -> False, "aab" -> True, "carerac" -> True.
 
+//HashSet will be the faster one.
+public class Solution {
+    public boolean canPermutePalindrome(String s) {
+        //HashSet
+        Set<Character> set = new HashSet<>();
+        for(int i = 0; i < s.length(); i++) {
+            if (!set.add(s.charAt(i))) {
+                set.remove(s.charAt(i));
+            }
+        }
+        if (set.size() <= 1) return true;
+        return false;
+    }
+}
 
 //Just count the appearances of the characters. if odd appearances are greater than 1, then return false;
 public class Solution {
---
+++ Leetcode/LC_77_Combinations.java
@@ -0,0 +1,34 @@
+/*
+77. Combinations
+Given two integers n and k, return all possible combinations of k numbers out of 1 ... n.
+For example,
+If n = 4 and k = 2, a solution is:
+
+[
+  [2,4],
+  [3,4],
+  [2,3],
+  [1,2],
+  [1,3],
+  [1,4],
+]*/
+
+//use k - 1 to countdown
+public class Solution {
+    public List<List<Integer>> combine(int n, int k) {
+        List<List<Integer>> res = new ArrayList<List<Integer>>();
+        if (n <= 0 || k <= 0) return res;
+        dfs(n, 1, new ArrayList<Integer>(), k, res);
+        return res;
+    }
+    private void dfs(int n, int index, List<Integer> tmp, int k, List<List<Integer>> res) {
+        if (k == 0) {
+            res.add(new ArrayList<Integer>(tmp));
+        }
+        for (int i = index; i <= n; i++) {
+            tmp.add(i);
+            dfs(n, i + 1, tmp, k - 1, res);
+            tmp.remove(tmp.size() - 1);
+        }
+    }
+}
